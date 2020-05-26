2ba7024efc572dde40e786c0fd6ce8760e81f992
GINELLE PLAISIR <plaisirginelle@gmail.com>
2020-05-26 02:21:54 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,25 +1,20 @@
-require 'net/http'
+  
 require 'open-uri'
+require 'net/http'
 require 'json'
 
 class GetRequester
-  
-  URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
-  
-    def get_response_body
-    uri = URI.parse(URL)
+  def initialize(url)
+    @url = url
+  end
+
+  def get_response_body
+    uri = URI.parse(@url)
     response = Net::HTTP.get_response(uri)
     response.body
   end
-  
-  
+
   def parse_json
-  programs = JSON.parse(self.get_programs)
-  programs.collect do |program|
-    program["agency"]  
+    JSON.parse(get_response_body)
   end
-end
-  
-  
-  
 end
