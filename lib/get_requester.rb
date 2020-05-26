8b22ef3c2fdcc1dfe033b7aff0fa07c33bd22e15
GINELLE PLAISIR <plaisirginelle@gmail.com>
2020-05-26 02:20:02 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -13,4 +13,13 @@ class GetRequester
   end
   
   
+  def parse_json
+  programs = JSON.parse(self.get_programs)
+  programs.collect do |program|
+    program["agency"]  
+  end
+end
+  
+  
+  
 end
